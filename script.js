cb0fd5be7cee7a715a3476b62879c2b6d613348b
clivepato93 <clivetoniato1@hotmail.com>
2022-04-18 16:57:18 +0100

    testing cursor

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 
+const main = document.querySelector('main');
+const dice = document.querySelector('.dice');
+
+
+
+function renderHTML(data){
+    console.log(data.id, data.advice)
+    const html =  `<p class="title">Advice #<span id="advice-id">${data.id}</span></p>
+    <p class="text">${data.advice}</p>`
+    main.insertAdjacentHTML('afterbegin',html)
+}
 
 const getAdvice = function(){
-    fetch(`	https://api.adviceslip.com/advice`).then(function (response) {
-    
-    console.log(response);
-    return response.json();
+    fetch(`https://api.adviceslip.com/advice/${117}`).then(function (response) {
+        
+        console.log(response);
+        return response.json();
     }).then(function (data) {
         const {slip} = data
+        renderHTML(slip)
         console.log(slip)
+        return slip
+        
     })
 }
 
-getAdvice()
+console.log(getAdvice())
+
+dice.addEventListener('click',function () {
+    main.style.color ='pink';
+})
